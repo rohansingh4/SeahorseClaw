d8a4f31a9fcfcf9e4ac1d830645e144808415801
Rohan Singh <rohan.singh1104@gmail.com>
2022-11-07 20:42:41 +0530

    All changes day 1

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function()){
+    //canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width=500;
+    canvas.height=500;
+    class InputHandler{
+
+    }
+    class Projectile{
+
+    }
+    class Particle{
+
+    }
+    class Player{
+        constructor(game){
+            this.game=game;
+            this.width=120;
+            this.height=190;
+            this.x=20;
+            this.y=100;
+            this.speedY=0;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect((this.x, this.y, this.width, this.height));
+        }
+
+    }
+    class Enemy{
+
+    }
+    class Layer{
+
+    }
+    class Background{
+
+    }
+    class UI{
+
+    }
+    class Game{
+
+    }
+};
